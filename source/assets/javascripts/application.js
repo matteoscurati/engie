81401902f4842dc89fd40f195c18953f847fea03
Matteo Scurati <matteo.scurati@gmail.com>
2017-01-28 23:02:26 +0100

    Half coloured

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -8,4 +8,8 @@ $(document).ready(() => {
   if ($('.half-article--same-height').length > 0) {
     $('.half-article--same-height').matchHeight();
   }
+
+  if ($('.half-coloured--same-height').length > 0) {
+    $('.half-coloured--same-height').matchHeight();
+  }
 });
